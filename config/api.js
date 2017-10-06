538fd63c04d3f2f07ab82b978b5467cb3d44e0cb
pensarfeo <dryto25@hotmail.com>
2017-10-06 14:38:13 +0200

    remove authorization header

--- config/api.js
+++ config/api.js
@@ -7,5 +7,5 @@ axios.defaults.params.apikey = 'vuRSkbbMpqujS0F2yhkuFnDVJ32znJIN'
 axios.defaults.baseURL = 'https://qover-test.apigee.net'
 axios.defaults.headers.post[ 'Content-Type' ] = 'application/json'
 axios.defaults.headers.post[ 'Qover-Api-Version' ] = '1.0'
-axios.defaults.headers.post.Authorization = `Bearer ${ token }`
+// axios.defaults.headers.post.Authorization = `Bearer ${ token }`
 axios.defaults.transformRequest = [ (data, headers) => JSON.stringify(data) ]
